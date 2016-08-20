57825b0f60bc8498fa4b34f4d895338204b7de90
munozyanez <munoz.yanez@gmail.com>
2016-08-20 13:31:13 +0200

    modified:   lib/fcontrol 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
+#include <complex>
 
 #include "fcontrol.h"
 
 int main()
 {
+
+    std::complex<double> a;
+    TimeSignal e;
+
+    for (int i=0; i<e.getN(); i++)
+    {
+        e.data[i]=sin(0.1*i*e.getDts());
+        std::cout << e.data[i];
+
+    }
+    std::cout << std::endl;
+
+    //SystemBlock
+    double num[]={1,0};
+    SystemBlock control;
+
+    control.SignalParams(e);
+
+
     return 0;
 }
