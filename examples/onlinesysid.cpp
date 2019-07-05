8eb664dadc7aeff0396a0fd6c538613c23294a62
HumaSOFT <humasoft@ing.uc3m.es>
2019-07-05 18:53:29 +0200

    modificado:    examples/onlinesysid.cpp 	modificado:    lib/fcontrol

--- examples/onlinesysid.cpp
+++ examples/onlinesysid.cpp
@@ -20,14 +20,17 @@ int main()
 
     IPlot real,id;
     double dts=0.01;
-    double in,out;
+    double in=0,out=0;
+    double tmax=20;
 
-    for (double t=0; t<20; t+=dts)
+    for (double t=0; t<tmax; t+=dts)
 
     {
-        in=1+0.01*((rand() % 10 + 1)-5);
-        out=in > sys;
+        in=1+0.001*((rand() % 10 + 1)-5); //u_{i-1}
+        out=in > sys;//y_{i}
+
         Gz.UpdateSystem(in,out);
+
         real.pushBack(out);
         //Gz.PrintParamsVector();
 
@@ -39,10 +42,9 @@ int main()
 
     vector<double> num(numOrder+1),den(denOrder+1);
     Gz.GetZTransferFunction(num,den);
-    cout << num[1] << endl;
     SystemBlock idsys(num,den);
 
-    for (double t=0; t<10; t+=dts)
+    for (double t=0; t<tmax; t+=dts)
 
     {
         in=1;//*(rand() % 10 + 1)-5;
