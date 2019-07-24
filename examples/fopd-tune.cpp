337ab91ab27089259bdc86fdae06d0e5a3a2a760
munozyanez <munoz.yanez@gmail.com>
2019-07-24 18:20:09 +0200

    modificado:     examples/fopd-tune.cpp 	modificado:     lib/fcontrol

--- examples/fopd-tune.cpp
+++ examples/fopd-tune.cpp
@@ -10,5 +10,20 @@ int main()
 {
 
 
+    SystemBlock sys(vector<double>{1,1},vector<double>{-199,201});
+
+    int numOrder=sys.GetNumOrder(),denOrder=sys.GetDenOrder();
+
+    double mag,phi;
+    double dts=0.01;
+    sys.GetMagnitudeAndPhase(dts,1,mag,phi);
+
+    cout << "mag: " << mag << " phi: " << phi << endl << "mag dB: " << 20*log10(mag) << " phi_deg: " << phi*180/M_PI <<endl;
+
+    FPDTuner tuner(60,20);
+    FPDBlock con;
+
+    tuner.TuneIsom(sys,con);
+
 
 }
