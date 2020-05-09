0f27aa40b58385059ee6045f1a24ae75e1c95dd4
humasoft <humasoft@humasoft.ing.uc3m.es>
2020-05-09 13:37:31 +0200

    modificado:     lib/fcontrol 	modificado:     src/fractional.cpp

--- src/fractional.cpp
+++ src/fractional.cpp
@@ -12,13 +12,13 @@ int main()
 
 
 //    FractionalController1DOF f1(-1.5,0.01);
-    FPDBlock f1(0,1,-1.5,0.02);
+    FPDBlock f1(0,1,-1.1,0.02);
 //    PIDBlock f2(0,1,0,0.02);
 
-    for (int i=0; i<10; i++)
+    for (int i=0; i<100; i++)
     {
         10 > f1;
-        cout << "State: " << f1.GetState() << endl;
+//        cout << "State: " << f1.GetState() << endl;
 
     }
     cout << "Final State: " << f1.GetState() << endl;
